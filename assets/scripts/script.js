1042cf63c46df208d8c882ff30b86e68b95c8fda
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-22 20:54:15 -0300

    STRUCTURE AND STYLING FINISHED I started this new project and I have no ideia how I will make this. I set some keyframes to move the images and manipulated the imgel to add 'slide-left' in each of one these imgs.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,15 @@
-alert('The script is running!')
+const sectionEl = document.querySelector('#img-slider');
+const imgEl = document.querySelectorAll('#img-el');
+const btnNextEl = document.querySelector('#btn-next');
+const btnPrevEl = document.querySelector('#btn-prev');
+
+// Setting attribute 'src' pictures to each img element;
+imgEl.forEach((img) => {
+  img.classList.add('slide-left');
+});
+
+const plusSlide = (number) => {
+  number = btnNextEl.classList.value();
+}
+
+
