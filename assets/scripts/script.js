2b60a4ba30bca5558ef518b9a407eb1e5e1f5ee0
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-23 18:22:42 -0300

    PROJECT FINISHD This time I coudn't do this project by my way. I watched the solution because I hadn't idea how to do the logic of this project. The logic of this project is that: There is an img wrapper thats envolves all images. The funcionality to move will work in it. After it, we set a variable called 'currentImg' to control a number to indentify the current img inside the nodelist. Then, we created two events triggers to the next button and previous button. Ithis events we add or subtract the currentImg value, with increment of decrement value, and then we invoke the function updateImg(). The updateImg() function has an if/else statement that checks if current value are greater than imgsEl(nodelist containing images) lenght, current value will be equals 1, if current value are smaller than one, the current value will be equal imgsEl.lenght. The final part of this function is a setTimeOut() Method, that changes the images everey 3000 milisecons, or 3 seconds. Preventining to create more than one intervals, we clear this interval with clearTimeOut() method inside each button's events that we trigged. To finished it, I add a simple function to generate randomly an rgba() color to body's background

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,56 @@
 const sectionEl = document.querySelector('#img-slider');
-const imgEl = document.querySelectorAll('#img-el');
+const imgsEl = document.querySelectorAll('#img-el');
 const btnNextEl = document.querySelector('#btn-next');
 const btnPrevEl = document.querySelector('#btn-prev');
+const bodyEl = document.querySelector('body');
+const imageContainerEl = document.querySelector(".image-container")
 
-// Setting attribute 'src' pictures to each img element;
-imgEl.forEach((img) => {
-  img.classList.add('slide-left');
-});
+// Img control;
+let currentImg = 1;
 
-const plusSlide = (number) => {
-  number = btnNextEl.classList.value();
+let timeout;
+
+btnNextEl.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout)
+  updateImg(imageContainerEl);
+  randomRGB(bodyEl);
+})
+
+btnPrevEl.addEventListener("click", () => {
+  currentImg--
+  clearTimeout(timeout)
+  updateImg(imageContainerEl);
+  randomRGB(bodyEl);
+})
+
+// Setting a random number to use in rgba() color;
+const randomRGB = (element) => {
+  let R = Math.floor(Math.random() * 255);
+  let G = Math.floor(Math.random() * 255);
+  let B = Math.floor(Math.random() * 255);
+
+  return element.style.backgroundColor = `rgba(${R}, ${G}, ${B}, .5)`;
 }
+randomRGB(bodyEl);
+
+updateImg();
+
+function updateImg() {
+
+  if(currentImg > imgsEl.length) {
+    currentImg = 1;
+  } else if(currentImg < 1) {
+    currentImg = imgsEl.length;
+  }
+  
+  imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 600}px)`;
+
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+    randomRGB(bodyEl);    
+  }, 3000)
+};
 
 
